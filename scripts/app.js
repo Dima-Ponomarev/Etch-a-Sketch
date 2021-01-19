f8e3273ee209dfd2326db204acedede22a0696b4
Dima Ponomarev <kikkyow18@gmail.com>
2021-01-19 12:47:59 +0400

    Added options and basic styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
-const gridWrapper = document.querySelector('.grid-wrapper');
-let gridSize = 16;
+
+
+//----------------FUNCTIONS----------------//
 
 const createGrid = (parentElement, size, hoverFunction) => {
     const parentWidth = parentElement.clientWidth;
@@ -10,10 +11,10 @@ const createGrid = (parentElement, size, hoverFunction) => {
             newDiv.className = `grid-element line-${i}`;
             newDiv.style.cssText = 
                 `display: inline-block;
-                margin-bottom: -2px;
                 border: 1px solid #ddd;
                 width: ${elementWidth}px;
-                height: ${elementWidth}px;`;
+                height: ${elementWidth}px;
+                background-color: white;`;
             
             newDiv.addEventListener("mouseover", (e) => hoverFunction(e));
             parentElement.appendChild(newDiv);
@@ -21,9 +22,82 @@ const createGrid = (parentElement, size, hoverFunction) => {
     }
 }
 
-const hoverBlack = e =>{
-    e.target.style.background = "black";
+
+const hoverHandlerBlack = e =>{
+    e.target.style.backgroundColor = "black";
 }
 
+const hoverHandlerRainbow = e =>{
+    e.target.style.backgroundColor = rainbowPalette[rainbowCounter % rainbowPalette.length];
+    rainbowCounter++;   
+}
 
-createGrid(gridWrapper, gridSize, hoverBlack);
+const hoverHandlerShadow = e =>{
+    const rgb = window.getComputedStyle(e.target).backgroundColor.match(/\d+/g);
+    const newRgb =  parseInt(rgb[0], 10) - 25; 
+    e.target.style.backgroundColor = `rgb(${newRgb}, ${newRgb}, ${newRgb})`; 
+}
+
+const resetClickHandler = () =>{
+    const newGridSize = prompt("Enter new grid size (max: 100)", 16);
+    if(newGridSize > 100){
+        alert("Max grid size exceeded! New size set to 100")
+        gridSize = 100;
+    } else {
+        gridSize = newGridSize;
+    }
+    gridWrapper.innerHTML = "";
+    createGrid(gridWrapper, gridSize, currentHoverEvent);
+}
+
+const defaultClickHandler = () =>{
+    headerText.className = "default-header";
+    currentHoverEvent = hoverHandlerBlack;
+    gridWrapper.innerHTML = "";
+    createGrid(gridWrapper, gridSize, currentHoverEvent);
+}
+
+const rainbowClickHandler = () =>{
+    headerText.className = "rainbow-header";
+    currentHoverEvent = hoverHandlerRainbow;
+    gridWrapper.innerHTML = "";
+    createGrid(gridWrapper, gridSize, currentHoverEvent);
+}
+
+const shadowClickHandler = () =>{
+    headerText.className = "shadow-header";
+    currentHoverEvent = hoverHandlerShadow;
+    gridWrapper.innerHTML = "";
+    createGrid(gridWrapper, gridSize, currentHoverEvent);
+}
+
+//----------------EVENT LISTENERS----------------//
+
+const gridWrapper = document.querySelector('.grid-wrapper');
+const headerText = document.querySelector('h1');
+const resetBtn = document.querySelector('.reset-btn');
+const defaultHoverButton = document.querySelector('.default-option');
+const rainbowHoverButton = document.querySelector('.rainbow-option');
+const shadowHoverButton = document.querySelector('.shadow-option');
+let currentHoverEvent = hoverHandlerBlack;
+let gridSize = 16;
+let rainbowCounter = 0;
+
+const rainbowPalette = [
+    '#ff0000', 
+    '#ffa500',
+    '#ffff00',
+    '#008000',
+    '#0000ff',
+    '#4b0082',
+    '#ee82ee'
+]
+
+
+resetBtn.addEventListener('click', resetClickHandler);
+defaultHoverButton.addEventListener('click', defaultClickHandler);
+rainbowHoverButton.addEventListener('click', rainbowClickHandler);
+shadowHoverButton.addEventListener('click', shadowClickHandler);
+
+
+createGrid(gridWrapper, gridSize, currentHoverEvent);
